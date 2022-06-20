078557e7b6577dfc4cd21836cb275390ff05dabb
ZainabOlaitan26 <zhaynabmonisola@gmail.com>
2022-06-20 07:47:43 -0700

    Exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    cmpt = 0
-    for index in range(x):
+    count = 0
+
+    for i in range(x):
         try:
-            print(my_list[index], end='')
-            cmpt += 1
-        except Exception as error:
+            print("{}".format(my_list[i]), end='')
+        except:
             break
-        print('')
-        return cmpt
+        else:
+            count += 1
+
+    print()
+    return (count)
